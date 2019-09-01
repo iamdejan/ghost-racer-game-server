caedb74081a27f9b2e497ba2d81afa0169836bf9
Giovanni Dejan <wb-giovannid529582@antfin.com>
2019-09-01 10:49:14 +0700

    Change from private to public

--- pkg/model/roomeventbuilders.go
+++ pkg/model/roomeventbuilders.go
@@ -1,39 +1,39 @@
 package model
 
 type roomEventWithPayload struct {
-	eventType roomEventType
+	EventType roomEventType
 
 	Payload interface{}
 }
 
 func (re roomEventWithPayload) Type() roomEventType {
-	return re.eventType
+	return re.EventType
 }
 
 type roomEventWithoutPayload struct {
-	eventType roomEventType
+	EventType roomEventType
 }
 
 func (re roomEventWithoutPayload) Type() roomEventType {
-	return re.eventType
+	return re.EventType
 }
 
 func RoomEventInsertPlayer(payload *PlayerJoinRoomEventPayload) RoomEvent {
 	return roomEventWithPayload{
-		eventType: playerJoinEventType,
+		EventType: playerJoinEventType,
 		Payload:   payload,
 	}
 }
 
 func RoomEventRemovePlayer(payload *PlayerLeaveRoomEventPayload) RoomEvent {
 	return roomEventWithPayload{
-		eventType: playerLeaveEventType,
+		EventType: playerLeaveEventType,
 		Payload:   payload,
 	}
 }
 
 func RoomEventRaceStarts() RoomEvent {
 	return roomEventWithoutPayload{
-		eventType: raceStartsEventType,
+		EventType: raceStartsEventType,
 	}
 }
